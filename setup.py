8c0a5b0123019acd5e082d0842f8b980fe5fe365
Andrei Lapets <a@lapets.io>
2022-03-22 23:39:08 -0400

    Test using pytest; update README, package files, and GitHub Actions workflows.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 # The lines below can be parsed by `docs/conf.py`.
 name = "symbolism"
-version = "0.2.3"
+version = "0.3.0"
 
 setup(
     name=name,
@@ -20,6 +20,4 @@ setup(
                 "expressions that can be evaluated at a later time.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    test_suite="nose.collector",
-    tests_require=["nose"],
 )
